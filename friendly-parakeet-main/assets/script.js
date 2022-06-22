913d9b87fc52e96afe3746ff00cbd17ebb6394d9
Jay <jaydentripp@gmail.com>
2022-06-22 18:33:20 -0400

    added else if statements for selected criteria depending on user input

--- friendly-parakeet-main/assets/script.js
+++ friendly-parakeet-main/assets/script.js
@@ -25,20 +25,72 @@ var toUpper = function (x) {
 // Variable for uppercase letters
 CapLetter = Letter.map(toUpper)
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var get = document.querySelector("#generate");
 
 get.addEventListener("click", function () {
   ps = generatePassword()
   document.getElementById("password").placeholder = ps
 })
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// Generate password function
+function generatePassword() {
+  enter = parseInt(prompt("What would you like the length of your password to be? Choose between 8 to 128 characters."))
+  // Validation check 
+  if (!enter) {
+    alert("You must input a value!");
+  } else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("You must choose between 8 to 128 characters!"))
+
+    // Start input prompts
+  } else {
+    confirmCharacter = confirm("Do you want special characters in your password?")
+    confirmLowercase = confirm("Do you want Lowercase letters in your password?")
+    confirmNumber = confirm("Do you want numbers in your password?")
+    confirmUppercase = confirm("Do you want Uppercase letters in your password?")
+  }
+
+  // Statement for if user doesn't pick criteria 
+  if (!confirmUppercase && !confirmCharacter && !confirmLowercase && !confirmNumber) {
+    alert("You must enter a criteria into all criteria options for your password!")
+  }
+
+  // One criteria selected 
+  else if (confirmNumber) {
+    choices = number
+  }
+  else if (confirmCharacter) {
+    choices = character
+  }
+  else if (confirmLowercase) {
+    choices = Letter
+  }
+  else if (confirmUppercase) {
+    choices = space.concat(CapLetter)
+  }
+  // Two criteria selected 
+  else if (confirmLowercase && confirmUppercase) {
+    choices = alpha.concat(alpha2)
+
+  } else if (confirmNumber && confirmUppercase) {
+    choices = alpha.concat(alpha2)
+
+  } else if (confirmCharacter && confirmUppercase) {
+    choices = alpha.concat(alpha2)
+
+  } else if (confirmLowercase && confirmNumber) {
+    choices = alpha.contact(number)
+
+  } else if (confirmCharacter && confirmNumber) {
+    choices = alpha.contact(number)
+
+  } else if (confirmCharacter && confirmLowercase) {
+    choices = character.concat(alpha)
+  }
+  // Three criteria selected
+  else if (confirmUppercase && confirmNumber && confirmCharacter) {
+    choices = character.concat(number, alpha2)
+  }
+
+
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
