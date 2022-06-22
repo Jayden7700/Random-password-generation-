a1e0ee7b430316ece1fda95bc2401b77a9cdb39b
Jay <jaydentripp@gmail.com>
2022-06-22 13:49:04 -0400

    added special characters, numbers, and letter variables

--- friendly-parakeet-main/assets/script.js
+++ friendly-parakeet-main/assets/script.js
@@ -8,8 +8,13 @@ var confirmLowercase;
 
 // Start variables for user criteria 
 // Letter characters
-Letter = ["a"]
+Letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Numeric characters
+number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Special characters 
+character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", ",", "<", ".", ">", "/", "?"];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
